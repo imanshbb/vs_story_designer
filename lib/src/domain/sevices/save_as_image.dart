d8c0a87d384969001f7e359512a1f747d21b1c94
Iman.HO <www.seyyediman1234@gmail.com>
2024-09-16 11:06:06 +0330

    test6

--- lib/src/domain/sevices/save_as_image.dart
+++ lib/src/domain/sevices/save_as_image.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:gal/gal.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -8,14 +12,27 @@ Future takePicture(
     required saveToGallery,
     required fileName}) async {
   try {
+    /// converter widget to image
+    RenderRepaintBoundary boundary =
+        contentKey.currentContext.findRenderObject();
+
+    ui.Image image = await boundary.toImage(pixelRatio: 3);
+
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    Uint8List pngBytes = byteData!.buffer.asUint8List();
+
     /// create file
     final String dir = (await getApplicationDocumentsDirectory()).path;
     String imagePath = '$dir/${fileName}_${DateTime.now()}.png';
+    File capturedFile = File(imagePath);
+    await capturedFile.writeAsBytes(pngBytes);
 
     if (saveToGallery) {
-      await Gal.putImage(fileName);
-      print('object22222');
-      return true;
+      try {
+        await Gal.putImage(byteData.toString());
+      } catch (e) {
+        print(e);
+      }
     } else {
       return imagePath;
     }
