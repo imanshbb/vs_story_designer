d633c576fc4cadf8ad445f9668c0a3fe9a794769
Iman.HO <www.seyyediman1234@gmail.com>
2024-09-25 11:06:15 +0330

    test

--- lib/src/presentation/text_editor_view/widgets/font_selector.dart
+++ lib/src/presentation/text_editor_view/widgets/font_selector.dart
@@ -65,77 +65,80 @@ class FontSelector extends StatelessWidget {
       builder: (context, editorNotifier, controlNotifier,
           draggableWidgetNotifier, paintingNotifier, child) {
         return SizedBox(
-          height: 80,
+          height: 65,
           width: _size.width,
           // alignment: Alignment.center,
-          child: ListView.builder(
-            controller: editorNotifier.fontFamilyController,
-            itemCount: controlNotifier.fontList!.length,
-            scrollDirection: Axis.horizontal,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: ListView.builder(
+              controller: editorNotifier.fontFamilyController,
+              itemCount: controlNotifier.fontList!.length,
+              scrollDirection: Axis.horizontal,
 
-            // onPageChanged: (index) {
-            //   // editorNotifier.text = .
-            //   // editorNotifier.fontFamilyIndex = index;
-            //   HapticFeedback.heavyImpact();
-            // },
-            physics: const BouncingScrollPhysics(),
-            // allowImplicitScrolling: true,
-            // pageSnapping: false,
-            itemBuilder: (context, index) {
-              return AnimatedOnTapButton(
-                onTap: () {
-                  editorNotifier.fontFamilyIndex = index;
-                  editorNotifier.fontFamilyController.jumpToPage(index);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 21.0),
-                  child: Container(
-                    width: 80,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        // color: index == editorNotifier.fontFamilyIndex
-                        //     ? Colors.white
-                        //     : Colors.black.withOpacity(0.4),
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(
-                          colors: index == editorNotifier.fontFamilyIndex
-                              ? [
-                                  Colors.white,
-                                  Colors.white,
-                                ]
-                              : [
-                                  const Color(0xff274589),
-                                  const Color(0xff5AB7C2),
-                                ],
-                        )),
-                    child: Center(
-                      child: Text(
-                        text[index],
-                        textAlign: TextAlign.center,
-                        maxLines: 1,
-                        style: AppFonts.getTextThemeENUM(
-                                controlNotifier.fontList![index])
-                            .bodyLarge!
-                            .merge(const TextStyle(
-                                // fontFamily: controlNotifier.fontList![index],
-                                // package: controlNotifier.isCustomFontList
-                                //     ? null
-                                //     : 'vs_story_designer'
-                                ))
-                            .copyWith(
-                              fontSize: 17.0,
-                              color: index == editorNotifier.fontFamilyIndex
-                                  ? const Color(0xff274589)
-                                  : Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
+              // onPageChanged: (index) {
+              //   // editorNotifier.text = .
+              //   // editorNotifier.fontFamilyIndex = index;
+              //   HapticFeedback.heavyImpact();
+              // },
+              physics: const BouncingScrollPhysics(),
+              // allowImplicitScrolling: true,
+              // pageSnapping: false,
+              itemBuilder: (context, index) {
+                return AnimatedOnTapButton(
+                  onTap: () {
+                    editorNotifier.fontFamilyIndex = index;
+                    editorNotifier.fontFamilyController.jumpToPage(index);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 21.0),
+                    child: Container(
+                      width: 65,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          // color: index == editorNotifier.fontFamilyIndex
+                          //     ? Colors.white
+                          //     : Colors.black.withOpacity(0.4),
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                            colors: index == editorNotifier.fontFamilyIndex
+                                ? [
+                                    Colors.white,
+                                    Colors.white,
+                                  ]
+                                : [
+                                    const Color(0xff274589),
+                                    const Color(0xff5AB7C2),
+                                  ],
+                          )),
+                      child: Center(
+                        child: Text(
+                          text[index],
+                          textAlign: TextAlign.center,
+                          maxLines: 1,
+                          style: AppFonts.getTextThemeENUM(
+                                  controlNotifier.fontList![index])
+                              .bodyLarge!
+                              .merge(const TextStyle(
+                                  // fontFamily: controlNotifier.fontList![index],
+                                  // package: controlNotifier.isCustomFontList
+                                  //     ? null
+                                  //     : 'vs_story_designer'
+                                  ))
+                              .copyWith(
+                                fontSize: 18.0,
+                                color: index == editorNotifier.fontFamilyIndex
+                                    ? const Color(0xff274589)
+                                    : Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
         );
       },
