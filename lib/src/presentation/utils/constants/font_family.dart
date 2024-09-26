a18b450a84b2ce78951e1465764759a52f8fb8a4
Iman.HO <www.seyyediman1234@gmail.com>
2024-09-26 10:46:52 +0330

    test

--- lib/src/presentation/utils/constants/font_family.dart
+++ lib/src/presentation/utils/constants/font_family.dart
@@ -227,7 +227,7 @@ class AppFonts {
       case FontType.rubik:
         return const TextTheme(
           bodyLarge: TextStyle(
-            fontFamily: 'ّIranSans',
+            fontFamily: 'IranSans',
           ),
         );
       case FontType.handJet:
