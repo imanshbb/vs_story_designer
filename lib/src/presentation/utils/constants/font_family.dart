11ddcaf5b1652b4c377412037d171ade361e5be6
Iman.HO <www.seyyediman1234@gmail.com>
2024-09-26 09:40:44 +0330

    test

--- lib/src/presentation/utils/constants/font_family.dart
+++ lib/src/presentation/utils/constants/font_family.dart
@@ -227,7 +227,7 @@ class AppFonts {
       case FontType.rubik:
         return const TextTheme(
           bodyLarge: TextStyle(
-            fontFamily: 'alibabaBlackWoff2',
+            fontFamily: 'Lalezar',
           ),
         );
       case FontType.handJet:
